5e36dd03920f7161e6ab8f963279b8d187a460f2
A-Alex-ander <Dubrov.work@gmail.com>
2023-12-22 14:45:37 +0700

    HW-2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+const arr = [];
+if (number1 % 2 === 0) {
+  console.log(arr.push(number1) + " " + "Это четное число");
+} else {
+  console.log(arr.push(number1) + " " + "Это нечетное число");
+}
