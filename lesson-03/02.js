6c447296665f5e9eabad389e10a481a31498dd79
A-Alex-ander <Dubrov.work@gmail.com>
2023-12-23 13:39:04 +0700

    HW-3-2 version 3

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,11 +1,4 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-// console.log(words);
-// const foundIndex = words.indexOf("апельсин");
-// if (foundIndex > -1) {
-//   console.log("Ура! Нашел");
-// } else {
-//   console.log("Придется поискать в другом магазине…");
-// }
 if (words.indexOf("апельсин")) {
   console.log("Ура! Нашел");
 } else {
