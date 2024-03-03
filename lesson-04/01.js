a5c7dee151cbd941c8320cf122c31f06afb01d6f
A-Alex-ander <Dubrov.work@gmail.com>
2024-03-03 20:36:55 +0700

    HM-2

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -1,1 +1,14 @@
-// code
+const arr44 = "1, 48, -4, 5, 4, -3, -10, 2, 32, -23, 44";
+
+const xr = 5;
+
+const func = (a) => {
+  const arr = [];
+  while (a > 0) {
+    arr.push(a);
+    a--;
+  }
+  return arr;
+};
+
+console.log(func(7));
